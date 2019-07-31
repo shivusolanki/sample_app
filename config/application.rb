b1d3cc96cef32d4421ee91e02157f1ea48f604b4
shivusolanki <sanayaon@gmail.com>
2019-07-31 16:43:58 +0530

    progress till 7 chapter

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
+# Require the gems listed in Gemfile, including any gem
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
@@ -10,6 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.assets.enabled = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
