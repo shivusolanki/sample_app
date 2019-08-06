c8c960a518bef2adfe7c6fa7156732e0d1d586b3
shivusolanki <sanayaon@gmail.com>
2019-08-06 18:48:52 +0530

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '~> 6.0.0.rc2'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt',         '3.1.12'
 gem 'faker'
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.7.0'
 gem 'will_paginate',           '3.1.7'
 gem 'bootstrap-will_paginate', '1.0.0'
 # Use sqlite3 as the database for Active Record
@@ -31,6 +33,11 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
+group :production do
+  gem 'pg',  '0.20.0'
+  gem 'fog', '1.42'
+end
+
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,12 +1,12 @@
 class PasswordResetsController < ApplicationController
-	before_action :get_user, only: [:edit, :update]
-	before_action :valid_user, only: [:edit, :update]
-	before_action :check_expiration, only: [:edit, :update]
+	before_action :get_user,         only: [:edit, :update]
+  before_action :valid_user,       only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
 
-  def create
+   def create
     @user = User.find_by(email: params[:password_reset][:email].downcase)
     if @user
       @user.create_reset_digest
@@ -28,7 +28,6 @@ class PasswordResetsController < ApplicationController
       render 'edit'
     elsif @user.update_attributes(user_params)          
       log_in @user
-      @user.update_attribute(:reset_digest, nil)
       flash[:success] = "Password has been reset."
       redirect_to @user
     else
@@ -38,26 +37,27 @@ class PasswordResetsController < ApplicationController
 
   private
 
-   def user_params
+  def user_params
       params.require(:user).permit(:password, :password_confirmation)
     end
 
-  def get_user
-  	@user = User.find_by(email: params[:email])
-  end
-
-  def valid_user
-    unless (@user && @user.activated? &&
-            @user.authenticated?(:reset, params[:id]))
-     redirect_to root_url
+    def get_user
+      @user = User.find_by(email: params[:email])
     end
-  end
 
-  def check_expiration
-    if @user.password_reset_expired?
-      flash[:danger] = "Password reset has expired."
-      redirect_to new_password_reset_url
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
     end
-  end
+
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
 end
+
 
