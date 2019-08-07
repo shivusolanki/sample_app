619e7ef54907cdce05336fd913a8e8b69e123b84
shivusolanki <sanayaon@gmail.com>
2019-08-07 14:32:43 +0530

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -48,6 +48,20 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
   def user_params
@@ -55,14 +69,14 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please login first"
+  #  def logged_in_user
+  #   unless logged_in?
+  #     store_location
+  #     flash[:danger] = "Please login first"
 
-      redirect_to login_url
-    end
-  end
+  #     redirect_to login_url
+  #   end
+  # end
 
   def correct_user
     @user = User.find(params[:id])
